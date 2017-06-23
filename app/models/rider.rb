db1d935002f2be15963d2722d6e606df34179aad
Lia Forgione <liaforg@gmail.com>
2017-06-23 15:19:07 -0400

    part 4 and 5

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -3,9 +3,8 @@ class Rider < ApplicationRecord
   has_many :dragons
 
   validates :name, :sword, presence: true
-
-  # validates :nickname, presence: true
-  # validates :email, presence: true, uniqueness: true
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true
 
   def swing
     "#{name} brings #{sword} crashing down!"
